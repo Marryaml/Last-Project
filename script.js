7a70d9efc090aba9076fc49394655455e48a30fe
Marryaml <94197586+Marryaml@users.noreply.github.com>
2022-01-17 01:46:28 +0100

    last step

---
+++ script.js
@@ -0,0 +1,7 @@
+let navigation = document.getElementById('links');
+let burgerbutton = document.getElementById('burger-icon');
+
+burgerbutton.addEventListener('click', function(){
+    navigation.classList.toggle('active');
+}
+)
